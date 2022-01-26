0f1adddba87dd38a4cc2d80536fd7018b73bd668
farukwebcode21 <farukahmadmk@yahoo.com>
2022-01-26 17:38:21 +0600

    almost complete

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
+
+let result = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+console.log(result);
